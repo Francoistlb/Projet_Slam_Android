fa4743bea10cd50a074dcd7577c2bce239e0a194
Francoistlb <113359189+Francoistlb@users.noreply.github.com>
2023-04-24 18:00:14 +0200

    new version 2

--- app/src/main/java/com/example/myloginapp/Visiteur.java
+++ app/src/main/java/com/example/myloginapp/Visiteur.java
@@ -2,7 +2,10 @@ package com.example.myloginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Visiteur extends AppCompatActivity {
 
@@ -10,5 +13,47 @@ public class Visiteur extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visiteur);
+
+        Button Creationutton = findViewById(R.id.creer);
+        Button ConsulterButton = findViewById(R.id.consulter);
+        Button logoutButton = findViewById(R.id.logoutButton);
+
+        logoutButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // Ajoutez le code pour déconnecter l'utilisateur de votre application ici
+                Intent logout = new Intent(Visiteur.this, MainActivity.class);
+                startActivity(logout);
+                finish();
+            }
+        });
+
+        Creationutton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // Ajoutez le code pour déconnecter l'utilisateur de votre application ici
+                Intent logout = new Intent(Visiteur.this, VisiteurCreation.class);
+                startActivity(logout);
+                finish();
+            }
+        });
+
+        ConsulterButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // Ajoutez le code pour déconnecter l'utilisateur de votre application ici
+                Intent logout = new Intent(Visiteur.this, VisiteurConsultation.class);
+                startActivity(logout);
+                finish();
+            }
+        });
+
+
     }
 }
